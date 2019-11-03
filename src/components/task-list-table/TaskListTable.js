f10ddaaa81df3b1bcde4d7552cb6953963edb67c
Alicia Thomson <web.aliciat@gmail.com>
2019-11-03 15:52:31 -0500

    Created layout for task table

--- src/components/task-list-table/TaskListTable.js
+++ src/components/task-list-table/TaskListTable.js
@@ -1,13 +1,46 @@
 import React from "react";
-import { tasks } from '../../services/data.json';
+import { tasks, users } from '../../services/data.json';
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+function getUser(id, users) {
+  const user = users.filter(_user => _user.ID === id)
+  return user[0].firstname + " " + user[0].lastname
+}
 
 export default function TaskListTable() {
-  console.log(tasks)
   return (
-    <ul className="sidebar__navigation">
+    <table className="taskListTable">
+      <thead>
+        <tr>
+          <th></th>
+          <th>Assignee</th>
+          <th>Status</th>
+          <th></th>
+        </tr>
+      </thead>
       {tasks.map((task) => (
-        {task}
+        <tr>
+          <td>
+            {task.severity === 'enhancement' && <i className="fas fa-info"></i>}
+            {task.severity === 'normal' && <i className="fas fa-exclamation-triangle"></i>}
+            {task.severity === 'critical' && <i className="fas fa-exclamation"></i>}
+          </td>
+          <td>
+            <h4>{task.label}</h4>
+            <p>Created by {getUser(task.author, users)}</p>
+          </td>
+          <td>
+            <h4>{getUser(task.assignee, users)}</h4>
+          </td>
+          <td>
+            <i className="fas fa-arrow-up"></i>
+            <FontAwesomeIcon icon={faArrowUp} />
+            <i className="fas fa-arrow-down"></i>
+          </td>
+         </tr>
       ))}
-    </ul>
+    </table>
   )
 }
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,7 +1,7 @@
 import React from "react";
 import TaskListTable from "../components/task-list-table/TaskListTable";
 
-export default function Dashboard() {
+export default function Project() {
   return (
     <div className="mainContainer">
       <h1>Project</h1>
