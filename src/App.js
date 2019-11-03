63c065edda991fb536b0c50482b3c78516f1cad8
Alicia Thomson <web.aliciat@gmail.com>
2019-11-03 11:55:31 -0500

    Built out the sidebar

--- src/App.js
+++ src/App.js
@@ -9,23 +9,10 @@ import Dashboard from './pages/Dashboard';
 import Project from './pages/Project';
 import "./styles/base/base.scss";
 
-const mainNav = [
-  {
-    text: 'Dashboard',
-    path: '/',
-    view: <Dashboard />
-  },
-  {
-    text: 'Project',
-    path: '/project',
-    view: <Project />
-  },
-]
-
 function App() {
   return (
     <Router>
-      <SideBarNav mainNav={mainNav} />
+      <SideBarNav />
       <Switch> 
         <Route key="dashboard" exact path="/">
           <Dashboard />
--- src/components/sidebar-nav/SideBarNav.js
+++ src/components/sidebar-nav/SideBarNav.js
@@ -2,16 +2,51 @@ import React from "react";
 import {
   Link
 } from "react-router-dom";
+import Logo from "../../assets/images/Logo.svg";
 
 export default function SideBarNav(props) {
-  const { mainNav } = props;
+  
+  const mainNav = [
+    {
+      text: 'Dashboard',
+      icon: '',
+      path: '/'
+    },
+    {
+      text: 'First Project',
+      icon: '',
+      path: '/project-1'
+    },
+    {
+      text: 'Second Project',
+      icon: '',
+      path: '/project-2'
+    },
+    {
+      text: 'Milestones',
+      icon: '',
+      path: '/milestones'
+    },
+    {
+      text: 'Settings',
+      iconClass: '',
+      path: '/settings'
+    },
+  ]
+  
   return (
-      <div className="sidebar">
-        <ul className="navigation">
-          {mainNav.map((navLink, index) => (
-            <li key={navLink.path}><Link to={navLink.path}>{navLink.text}</Link></li>
-          ))}
-        </ul>
-      </div>
+    <div className="sidebar">
+      <img className="logo" alt="TrakTime Logo" src={Logo}/>
+      <ul className="navigation">
+        {mainNav.map((navLink) => (
+          <li key={navLink.path}>
+            <Link to={navLink.path}>
+              <i className={navLink.iconClass}></i>
+              {navLink.text}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
